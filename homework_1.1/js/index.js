1c58577cd87b688bbdee8459134a950763f9cf56
ananas <anastasia.avanheim@gmail.com>
2022-05-24 15:16:27 +0300

    commited

---
+++ homework_1.1/js/index.js
@@ -0,0 +1,2 @@
+const user_name = prompt('What is your name?');
+alert("Hello, " + user_name + "! How are you?");
---
+++ homework_1.2/js/index.js
@@ -0,0 +1,4 @@
+let first = prompt("1", '');
+let second = prompt("2", '');
+let sum = parseInt(first) + parseInt(second);
+alert(sum);
